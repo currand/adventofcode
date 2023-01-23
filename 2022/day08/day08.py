cd69b9c5c6010521ce8c5f73f42870c335f2e98c
David Curran <currand@gmail.com>
2023-01-23 18:45:14 -0500

    Day 8-1

--- 2022/day08/day08.py
+++ 2022/day08/day08.py
@@ -3,97 +3,93 @@ from pprint import pp
 from typing import Generator, List, Union, Iterator
 
 class Grid():
-    def __init__(self, h: int, w: int) -> None:
-        self.grid = [[None for c in range(0, w)] for r in range(0, h)]
+    def __init__(self, arr: list[list[int]]) -> None:
+        self.w = len(arr[0]) - 1
+        self.h = len(arr) - 1
+        self.grid = arr
     
-    def __repr__(self) -> str:
+    def __repr__(self) -> str | None:
         pp(self.grid)
 
-    def add_coord(self, r: int, c: int, data: int) -> None:
+    def set_coord(self, r: int, c: int, data: int) -> None:
         self.grid[r][c] = data
 
-    def get_coord(self, r: int, c: int) -> int:
+    def get_coord(self, r: int, c: int) -> int | None:
         return self.grid[r][c]
 
-    def get_column(self, c: int):
-        return {(row, col[c]) for row, col in enumerate(self.grid)
+    def get_column(self, c: int) -> list[tuple[int,int,int]]:
+        out: list[tuple[int,int,int]] = []
+        for i,r in enumerate(self.grid):
+            out.append((i, c, self.grid[i][c]))
+        return out
 
-def get_column(arr: list[list[int]], c: int) -> list:
-    return [(row, col[c]) for row, col in enumerate(arr)]
+    def get_row(self, r: int):
+        return [(r, c, self.grid[r][c]) for c in range(0,len(self.grid[r]))]
 
-def circle_grid(arr: list[list[int]], h: int, w: int,
-                remove_corners: bool = False) -> Generator:
-    """
-    Return a generator that yields x,y coordinates
-    circling the outside of a 2d grid from 0,0
-    in the top left moving clockwise
+    def circle_grid(self, remove_corners: bool = False) -> Generator:
+        """
+        Return a generator that yields x,y coordinates
+        circling the outside of a 2d grid from 0,0
+        in the top left moving clockwise
 
-    Args:
-        arr (list): _description_
-        h (int): _description_
-        w (int): _description_
+        Args:
+            arr (list): _description_
+            h (int): _description_
+            w (int): _description_
 
-    Returns:
-        Iterable: _description_
-    """
-    coords: list[tuple[int, int]] =  []
-    coords += [(0, c) for c in range(1, w+1)]
-    coords += [(r, w) for r in range(1, h+1)]
-    coords += [(h, c) for c in range (w, -1, -1)]
-    coords += [(0, r) for r in range (h, -1, -1)]
+        Returns:
+            Iterable: _description_
+        """
+        coords: list[tuple[int, int]] =  []
+        coords += [(0, c) for c in range(1, self.w+1)]
+        coords += [(r, self.w) for r in range(1, self.h+1)]
+        coords += [(self.h, c) for c in range (self.w, -1, -1)]
+        coords += [(0, r) for r in range (self.h, -1, -1)]
 
-    if remove_corners:
-        coords = [x for x in coords if x not in get_corners(arr, h, w)]
-    
-    for item in coords:
-        yield item
+        if remove_corners:
+            coords = [x for x in coords if x not in self.get_corners()]
+        
+        for item in coords:
+            yield item
 
-def get_corners(arr: list[list[int]], h: int, w: int) -> list[tuple[int, int]]:
-    return [(0,0), (0, w), (h, w), (h, 0)]
+    def get_corners(self) -> list[tuple[int, int]]:
+        return [(0,0), (0, self.w), (self.h, self.w), (self.h, 0)]
 
-def side(h: int, w: int, coord: tuple[int, int]) -> str | None:
+def side(grid, coord: tuple[int, int]) -> str | None:
     if coord[0] == 0:
         return "TOP"
-    elif coord[0] == h:
+    elif coord[0] == grid.h:
         return "BOTTOM"
     elif coord[1] == 0:
         return "LEFT"
-    elif coord[1] == w:
+    elif coord[1] == grid.w:
         return "RIGHT"
     else:
         return None
 
-def count_trees(arr: list[tuple[int, int]], how: str) -> list[int]:
+def count_trees(arr: list[tuple[int, int, int]]) -> list[int]:
     tall_trees = []
-    if how == 'r':
-        curr_min = arr.pop(0)[0]
-        for coord in arr[:-1]:
-            if coord[0] > curr_min:
-                tall_trees.append(coord)
-                curr_min = coord[0]
-    else:
-        curr_min = arr.pop(0)[1]
-        for coord in arr[:-1]:
-            if coord[1] > curr_min:
-                tall_trees.append(coord)
-                curr_min = coord[1]
+    curr_min = arr.pop(0)[2]
+    for coord in arr[:-1]:
+        if coord[2] > curr_min:
+            tall_trees.append((coord[0], coord[1]))
+            curr_min = coord[2]
     
     return tall_trees
-
 
 if __name__ == '__main__':
     filedir = os.path.dirname(__file__)
     day = filedir[-2:]
     infile = filedir + f'/day{day}.txt'
     testfile = filedir + f'/test{day}.txt'
-    lines = [x.strip() for x in open(testfile)]
+    lines = [x.strip() for x in open(infile)]
 
     forrest = [[int(j) for j in i] for i in [list(line) for line in lines]]
-    w = len(forrest[0]) - 1
-    h = len(forrest) - 1
+    grid = Grid(forrest)
+
     trees = []
     outside = 4
-    for tree in circle_grid(forrest, h, w, remove_corners=True):
+    for tree in grid.circle_grid(remove_corners=True):
         ''' 
         1. start from top left
         2. move clockwise using circle_crid()
@@ -104,19 +100,20 @@ if __name__ == '__main__':
             4. If side() = LEFT, get row, count trees taller than item 0
         '''
 
-        outside += 1
-        if side(h, w, tree) == 'TOP':
-            column = get_column(forrest, tree[1])
-            trees += count_trees(column, 'c')
-        elif side(h, w, tree) == 'RIGHT':
-            row = [(tree[0], c) for c in forrest[tree[0]:-1]]
-            count  += count_trees(row[-2:0:-1], min_h=row[-1])
-        elif side(h, w, tree) == 'BOTTOM':
-            column = get_column(forrest, tree[1])
-            count  += count_trees(column[-1:1:-1], min_h=column[-1])
-        elif side(h, w, tree) == 'LEFT':
-            row = forrest[tree[0]]
-            count  += count_trees(row[1:], min_h=row[0])
-        
-    print(count + outside)
+        outside += 1        
+        if side(grid, tree) == 'TOP':
+            column = grid.get_column(tree[1])
+            trees += count_trees(column)
+        elif side(grid, tree) == 'RIGHT':
+            row = grid.get_row(tree[0])
+            trees  += count_trees(row[::-1])
+        elif side(grid, tree) == 'BOTTOM':
+            column = grid.get_column(tree[1])
+            trees  += count_trees(column[::-1])
+        elif side(grid, tree) == 'LEFT':
+            row = grid.get_row(tree[0])
+            trees  += count_trees(row)
+    
+    
+    print('trees: {}, outside: {}, {}'.format(len(set(trees)), outside, len(set(trees)) + outside))
     pass
