d6fadbe12e076e88dcef8b86a8e996b5d9366893
David Curran <currand@gmail.com>
2023-01-22 22:15:02 -0500

    Working on day8-1

--- 2022/day08/day08.py
+++ 2022/day08/day08.py
@@ -1,8 +1,25 @@
 import os
+from pprint import pp
 from typing import Generator, List, Union, Iterator
 
+class Grid():
+    def __init__(self, h: int, w: int) -> None:
+        self.grid = [[None for c in range(0, w)] for r in range(0, h)]
+    
+    def __repr__(self) -> str:
+        pp(self.grid)
+
+    def add_coord(self, r: int, c: int, data: int) -> None:
+        self.grid[r][c] = data
+
+    def get_coord(self, r: int, c: int) -> int:
+        return self.grid[r][c]
+
+    def get_column(self, c: int):
+        return {(row, col[c]) for row, col in enumerate(self.grid)
+
 def get_column(arr: list[list[int]], c: int) -> list:
-    return [row[c] for row in arr]
+    return [(row, col[c]) for row, col in enumerate(arr)]
 
 def circle_grid(arr: list[list[int]], h: int, w: int,
                 remove_corners: bool = False) -> Generator:
@@ -21,13 +38,15 @@ def circle_grid(arr: list[list[int]], h: int, w: int,
     """
     coords: list[tuple[int, int]] =  []
     coords += [(0, c) for c in range(1, w+1)]
+    coords += [(r, w) for r in range(1, h+1)]
     coords += [(h, c) for c in range (w, -1, -1)]
-    coords += [(0, c) for c in range (w, -1, -1)]
+    coords += [(0, r) for r in range (h, -1, -1)]
 
     if remove_corners:
         coords = [x for x in coords if x not in get_corners(arr, h, w)]
     
-    yield [item for item in coords]
+    for item in coords:
+        yield item
 
 def get_corners(arr: list[list[int]], h: int, w: int) -> list[tuple[int, int]]:
     return [(0,0), (0, w), (h, w), (h, 0)]
@@ -44,6 +63,23 @@ def side(h: int, w: int, coord: tuple[int, int]) -> str | None:
     else:
         return None
 
+def count_trees(arr: list[tuple[int, int]], how: str) -> list[int]:
+    tall_trees = []
+    if how == 'r':
+        curr_min = arr.pop(0)[0]
+        for coord in arr[:-1]:
+            if coord[0] > curr_min:
+                tall_trees.append(coord)
+                curr_min = coord[0]
+    else:
+        curr_min = arr.pop(0)[1]
+        for coord in arr[:-1]:
+            if coord[1] > curr_min:
+                tall_trees.append(coord)
+                curr_min = coord[1]
+    
+    return tall_trees
+
 
 if __name__ == '__main__':
     filedir = os.path.dirname(__file__)
@@ -53,9 +89,10 @@ if __name__ == '__main__':
     lines = [x.strip() for x in open(testfile)]
 
     forrest = [[int(j) for j in i] for i in [list(line) for line in lines]]
-    w = len(forrest[0])
-    h = len(forrest)
-
+    w = len(forrest[0]) - 1
+    h = len(forrest) - 1
+    trees = []
+    outside = 4
     for tree in circle_grid(forrest, h, w, remove_corners=True):
         ''' 
         1. start from top left
@@ -66,12 +103,20 @@ if __name__ == '__main__':
             3. If side() = BOTTOM, get column, count trees taller than item -1 in reverse order
             4. If side() = LEFT, get row, count trees taller than item 0
         '''
+
+        outside += 1
         if side(h, w, tree) == 'TOP':
-            column = get_column(forrest, tree(1))
+            column = get_column(forrest, tree[1])
+            trees += count_trees(column, 'c')
         elif side(h, w, tree) == 'RIGHT':
-            pass
+            row = [(tree[0], c) for c in forrest[tree[0]:-1]]
+            count  += count_trees(row[-2:0:-1], min_h=row[-1])
         elif side(h, w, tree) == 'BOTTOM':
-            pass
+            column = get_column(forrest, tree[1])
+            count  += count_trees(column[-1:1:-1], min_h=column[-1])
         elif side(h, w, tree) == 'LEFT':
-            pass
+            row = forrest[tree[0]]
+            count  += count_trees(row[1:], min_h=row[0])
+        
+    print(count + outside)
     pass
