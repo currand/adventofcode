e9ad28ef7833e60383664797825fe8002324ea59
David Curran <currand@gmail.com>
2024-10-15 22:13:01 -0400

    Day 4 part 2 not working

--- 2023/day04/day04b.py
+++ 2023/day04/day04b.py
@@ -1,6 +1,6 @@
 import os
 
-TEST = False
+TEST = True
 
 filedir = os.path.dirname(__file__)
 day = filedir[-2:]
@@ -9,7 +9,31 @@ if TEST:
 else:
     infile = filedir + f'/day{day}.txt'
 
-lines = [x.strip() for x in open(infile, 'r')]
+with open(infile, 'r') as f:
+    lines = [line.strip() for line in f]
 
-if __name__ == '__main__':
-    pass
+def parse_card(card: str) -> dict:
+    output = {}
+    name, numbers = card.split(':')
+    winners, nums_have = numbers.split(' | ')
+    output['name'] = int(name.replace('Card ', ''))
+    output['winners'] = {int(x) for x in winners.split()}
+    output['nums_have'] = {int(x) for x in nums_have.split()}
+    output['intersect'] = output['winners'].intersection(output['nums_have'])
+
+    return output
+
+max_card = len(lines)
+cards_to_process = list()
+cards_seen = dict()
+
+for line in lines:
+    card = parse_card(line)
+    won = len(card['intersect'])
+    if card['name'] not in cards_seen.keys():
+        cards_seen[card['name']] = 1
+    else:
+        cards_seen[card['name']] += 1
+    cards_to_process += range(card['name'] + 1, won + 2)
+
+pass
